87a7ce6ab2fcb6dac0c47a177864f343adfa91b9
Riley Trautman <rileyt@spiceworks.com>
2015-06-04 13:57:11 -0500

    added relations in migrate files

--- db/migrate/20150604141434_create_roles.rb
+++ db/migrate/20150604141434_create_roles.rb
@@ -1,6 +1,8 @@
 class CreateRoles < ActiveRecord::Migration
   def change
     create_table :roles do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :permission, index: true
 
       t.timestamps null: false
     end
