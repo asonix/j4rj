7e874aedfaf4e018afbc651aafbb29bdb5418585
Riley Trautman <rileyt@spiceworks.com>
2015-06-09 11:40:17 -0500

    Create basic recursive dropdown

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   helper_method :current_user
+  helper_method :serve_dropdown
   
-  before_filter :get_page_info
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
@@ -30,10 +29,66 @@ class ApplicationController < ActionController::Base
   def require_permission(permission)
     redirect_to '/' unless current_user.has_permission?(permission)
   end
-  
-  def get_page_info
-    @pages_list = Page.all
-    @info = @pages_list.pluck(:title, :url)
+
+  def serve_dropdown
+    @page_ids = []
+    return make_dropdown
+  end
+
+  def make_dropdown(page_title="", page_url="", page_id=nil)
+    output = ""
+    if page_id.nil?
+      info = Page.all.pluck(:title, :url, :id, :parent_page_id)
+    else
+      info = Page.where(parent_page_id: page_id).pluck(:title, :url, :id, :parent_page_id)
+      output += open_expand(page_title, page_url, page_id)
+    end
+    info.each do |title, url, id, parent|
+      if page_has_children(id)
+        unless @page_ids.include?(id)
+          output += make_dropdown(title, url, id)
+        end
+      else
+        if parent == page_id
+          unless @page_ids.include?(id)
+            puts "parent of #{title}: #{parent}"
+            puts "page_id of #{page_title}: #{page_id}"
+            output += make_item(title, url, id)
+          end
+        end
+      end
+    end
+    unless page_id.nil?
+      output += close_expand
+    end
+    return output
+  end
+
+  private
+  def page_has_children(id)
+    Page.exists?(parent_page_id: id)
   end
   
+  def open_expand(page_title, page_url, page_id)
+    @page_ids.push(page_id)
+    output  = "<div class=\"expand\">"
+    output += "<a href=\"/#{page_url}\">"
+    output += "<div class=\"title\">#{page_title}</div>"
+    output += "</a>"
+    output += "<div class=\"content hide\">"
+    return output
+  end
+
+  def close_expand
+    return "</div></div>"
+  end
+
+  def make_item(page_title, page_url, page_id)
+    @page_ids.push(page_id)
+    output  = "<a href=\"/#{page_url}\">"
+    output += "<div class=\"item\">#{page_title}</div>"
+    output += "</a>"
+    return output
+  end
+
 end
